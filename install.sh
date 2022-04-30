82d0e11ff74a608639e0f306afbdd54416affb84
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-05-01 01:17:18 +0300

    Update install.sh
    added the Unstable Repository importer

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ install="apt-get install"
 # Update the system
 sudo apt update && sudo apt upgrade -y
 
+# Import Sid/Unstable Repository
+echo "deb http://deb.debian.org/debian unstable main contrib non-free" | sudo tee -a /etc/apt/sources.list
+echo "deb-src http://deb.debian.org/debian unstable main contrib non-free" | sudo tee -a /etc/apt/sources.list
+
 # Install
  $install synaptic
