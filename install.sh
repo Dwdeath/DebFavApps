0e57cc13e055afaa2d64f7ba6ec97e430334a6d9
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-05-01 01:16:10 +0300

    Update install.sh
    added updater

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # Variables
 apps_list=https://dwdeath.github.io/DebFavApps/list.txt
 install="apt-get install"
+# Update the system
+sudo apt update && sudo apt upgrade -y
 
 # Install
  $install synaptic
